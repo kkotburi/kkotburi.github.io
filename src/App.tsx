a7220d6e24bf48987aefc592cdc9e8c0b9be90c7
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-09-22 23:06:27 +0900

    Chore/router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Router from './shared/Router';
 
-function App() {
-  return <div className="App" />;
-}
+const App = () => {
+  return <Router />;
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './reset.css';
-import { Provider } from 'react-redux';
-import store from './redux/config/configStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   // <React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <App />
   // </React.StrictMode>
 );
