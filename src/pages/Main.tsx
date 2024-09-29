4a4500d7d7348a10fc3a54fe7386e65a0f06c323
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-09-29 20:21:20 +0900

    Update/Project

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -6,9 +6,7 @@ const Main = () => {
       <div>이혜영</div>
       <div>Frond-end Developer</div>
       <div>꾸준히 자기 개발을 위해 1D1C(1 day 1 commit)을 실천하고 있습니다.</div>
-      <div>Skill</div>
       <div>내일배움캠프 수료</div>
-      <div>Project</div>
     </div>
   );
 };
