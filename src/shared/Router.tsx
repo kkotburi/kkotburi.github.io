faf613fdb2ada35b9e300f1a02b66644e5823a0f
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-09-30 21:44:08 +0900

    Update/Router

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../pages/Main';
+import Project from '../pages/Project';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/project" element={<Project />} />
       </Routes>
     </BrowserRouter>
   );
