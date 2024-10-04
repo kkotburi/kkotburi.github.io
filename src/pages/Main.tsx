1f3eabb086b00c98b15758e402f55bc2df3a6173
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-10-04 22:14:57 +0900

    Update/Main

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 const Main = () => {
   return (
     <div>
+      <div>
+        <div>Home</div>
+        <div>Project</div>
+      </div>
+      <div>Profile</div>
       <div>이혜영</div>
       <div>Frond-end Developer</div>
       <div>꾸준히 자기 개발을 위해 1D1C(1 day 1 commit)을 실천하고 있습니다.</div>
