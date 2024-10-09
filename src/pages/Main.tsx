30c8447ad5efe6fd38442fd25e900160026a1e1f
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-10-09 23:16:21 +0900

    Update/Main

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -13,7 +13,7 @@ const Main = () => {
       <div>이혜영</div>
       <div>Frond-end Developer</div>
       <button onClick={() => window.open('https://github.com/kkotburi')}>GitHub</button>
-      <div>Blog</div>
+      <button onClick={() => window.open('https://velog.io/@kkotburi/posts')}>Blog</button>
       <div>꾸준히 자기 개발을 위해 1D1C(1 day 1 commit)을 실천하고 있습니다.</div>
       <div>내일배움캠프 수료</div>
     </div>
