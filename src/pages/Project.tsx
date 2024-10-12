893ea5e01e92e848715aa411168ec608328b5c03
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-10-12 22:00:29 +0900

    Update/components

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Pyeonsik from '../components/Project.tsx/pyeonsik';
+import Pyeonsik from '../components/Project/Pyeonsik';
 
 const Project = () => {
   const navigate = useNavigate();
