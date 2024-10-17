718722de818938265ea95c8ec6afe38f4c97a1e6
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-10-17 23:41:02 +0900

    Update/Pyeonsik

--- src/components/Project/Pyeonsik.tsx
+++ src/components/Project/Pyeonsik.tsx
@@ -5,6 +5,7 @@ const Pyeonsik = () => {
     <div>
       <div>편식</div>
       <button onClick={() => window.open('https://github.com/kkotburi/project-pyeonsik')}>GitHub</button>
+      <button onClick={() => window.open('https://pyeonsik.vercel.app')}>서버 주소</button>
       <div>TypeScript</div>
       <div>편의점 음식을 조합하고 공유하는 커뮤니티</div>
       <div>
